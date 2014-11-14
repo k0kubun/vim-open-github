8a888a156aa4e4e8dfc424dbcef962e110d89652
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-14 22:36:37 +0900

    Fix spec_helper for passing test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 module VIM
   class << self
     def evaluate(*args)
-      # no need to work in test
+      ""
     end
 
     def command(*args)
-      # no need to work in test
+      ""
     end
   end
 end
